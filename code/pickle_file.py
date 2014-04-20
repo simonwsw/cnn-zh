9690162f6d2675504fbcbb83929169e7fc24a54d
Saiwen Wang <simonwsw@gmail.com>
2014-04-20 12:10:06 +0800

    [small_batch] pickle_file change to batch by batch

--- code/pickle_file.py
+++ code/pickle_file.py
@@ -13,80 +13,27 @@ class PickleFile(object):
 
     # the return size is subjected to the batch size
     # not all the data in the file will be returned
-    def read_file(self, file_dir, batch_size=None):
-
-        # train set
-        open_name = os.path.join(os.path.split(__file__)[0], "..", file_dir, \
-            "train.pkl.gz")
+    @staticmethod
+    def read_file(read_dir, read_prefix, batch_count):
+        read_name = (("%s(%i).pkl.gz") % (read_prefix, batch_count))
+        open_name = os.path.join(os.path.split(__file__)[0], "..", read_dir, 
+            read_name)
         with gzip.open(open_name, 'rb') as f:
-            pickle_array = cPickle.load(f)
+            set_x, set_y = cPickle.load(f)
 
-        # regulize for batch
-        if batch_size is None:
-            n_train = len(pickle_array[0])
-        else:
-            n_train_batches = len(pickle_array[0]) / batch_size
-            n_train = n_train_batches * batch_size
+        print len(set_x), set_x[0].shape
 
-        train_set_x, train_set_y = self.shared_dataset(
-            [pickle_array[0][:n_train], pickle_array[1][:n_train]])
-        print "File train.pkl.gz is loaded"
+        # change to share data set
+        shared_x = theano.shared(numpy.asarray(set_x, 
+            dtype=theano.config.floatX), borrow=True)
+        shared_y = theano.shared(numpy.asarray(set_y, 
+            dtype=theano.config.floatX), borrow=True)
 
-        # valid set
-        open_name = os.path.join(os.path.split(__file__)[0], "..", file_dir, \
-            "valid.pkl.gz")
-        with gzip.open(open_name, 'rb') as f:
-            pickle_array = cPickle.load(f)
-
-        # regulize for batch
-        if batch_size is None:
-            n_valid = len(pickle_array[0])
-        else:
-            n_valid_batches = len(pickle_array[0]) / batch_size
-            n_valid = n_valid_batches * batch_size
-        valid_set_x, valid_set_y = self.shared_dataset(
-            [pickle_array[0][:n_valid], pickle_array[1][:n_valid]])
-        print "File valid.pkl.gz is loaded"
-
-        # test set
-        open_name = os.path.join(os.path.split(__file__)[0], "..", file_dir, \
-            "test.pkl.gz")
-        with gzip.open(open_name, 'rb') as f:
-            pickle_array = cPickle.load(f)
-
-        # regulize for batch
-        if batch_size is None:
-            n_test = len(pickle_array[0])
-        else:
-            n_test_batches = len(pickle_array[0]) / batch_size
-            n_test = n_test_batches * batch_size
-        test_set_x, test_set_y = self.shared_dataset(
-            [pickle_array[0][:n_test], pickle_array[1][:n_test]])
-        print "File test.pkl.gz is loaded"
-
-        rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y),
-                (test_set_x, test_set_y)]
-        n_batches = [n_train_batches, n_valid_batches, n_test_batches]
-        return rval, n_batches
-
-    # use shared data to make faster copy between gpu and memory
-    def shared_dataset(self, data_xy, borrow=True):
-        data_x, data_y = data_xy
-        shared_x = theano.shared(numpy.asarray(data_x, \
-            dtype=theano.config.floatX), borrow=borrow)
-        shared_y = theano.shared(numpy.asarray(data_y, \
-            dtype=theano.config.floatX), borrow=borrow)
-        
-        # store data as int32 to make data copy more efficiently
+        print (("File %s is loaded") % (read_name))
         return shared_x, T.cast(shared_y, 'int32')
 
-    def print_info(self):
-        print len(self.pickle_array[0])
-        print self.pickle_array[0][0].shape
-
-def read_pickle():
-    pickle_file = PickleFile()
-    pickle_file.read_file("data")
+def test():
+    PickleFile.read_file("data/train_pickle", "train", 0)
 
 if __name__ == '__main__':
-    read_pickle()
+    test()
